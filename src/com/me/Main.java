9e7fd2602581be23b0965c83b922c14d95bb848d
patrickatcw <pj8392@yahoo.com>
2018-02-28 22:34:12 -0500

    initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,14 @@
+package com.me;
+/*
+*****NOTE: composition is about having things, as opposed to being things;
+creating objects within objects.
+composition is most likely to be used before inheritance!!
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
