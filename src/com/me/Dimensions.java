0e6269513e161ac86304b59fb27f6761a473695a
patrickatcw <pj8392@yahoo.com>
2018-02-28 23:04:21 -0500

    created class with fields, constructor, getters next step 23 is to make PC class note; error on dimensions gone

---
+++ src/com/me/Dimensions.java
@@ -0,0 +1,32 @@
+package com.me;
+//step 19
+public class Dimensions {
+
+    //step 20 fields
+    private int width;
+    private int height;
+    private int depth;
+
+    //step 21 constructor
+    public Dimensions(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    //step 22 getter
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
+
+//error in case class gone
+//next step 23 make PC class
