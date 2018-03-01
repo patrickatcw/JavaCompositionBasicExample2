7ca0490e3c9691a1051bc120061b5634b14623f5
patrickatcw <pj8392@yahoo.com>
2018-02-28 22:54:32 -0500

    created class with fields, constructor, getters next step 14 is to make monitor class note; error gone in monitor class

---
+++ src/com/me/Resolution.java
@@ -0,0 +1,26 @@
+package com.me;
+//step 10 create this class
+public class Resolution {
+
+    //step 11 fields
+    private int width;
+    private int height;
+
+    //step 12 constructor
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    //step 13 getters
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
+
+//step 14 create case class
+//error in monitor class gone
